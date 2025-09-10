0a7d31abb4eac74e458deab79575963ac4971cbd
HenryChipanaErgueta <chipanahenry697@gmail.com>
2025-09-10 15:36:49 -0400

    Add files via upload

---
+++ Códigos Turtle/EJERCICIO1.py
@@ -0,0 +1,19 @@
+import turtle
+
+v = turtle.Screen()
+t = turtle.Turtle()
+t.shape("turtle")
+t.color("blue")
+t.pensize(5)
+t.forward(70)
+t.left(100)
+t.forward(200)
+t.left(160)
+t.forward(200)  
+t.left(100)
+t.forward(70)
+
+
+turtle.done()
+v.exitonclick()
+
